3a74e98d549309f770d7f267ce37fbb82d158876
fullsirkool <64348207+fullsirkool@users.noreply.github.com>
2022-10-31 02:03:36 +0700

    fix tokenContract (ERC721)

--- TokenContract.sol
+++ TokenContract.sol
@@ -4,8 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract TokenContract is ERC721URIStorage {
+contract TokenContract is ERC721URIStorage, ReentrancyGuard {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
@@ -27,11 +28,22 @@ contract TokenContract is ERC721URIStorage {
     }
 
     function createToken(string memory tokenURI) public returns (uint256) {
-        require(_tokenIdCounter.current() <= MAX_SUPPLY, "I'm sorry number of token reached the cap");
+        require(_tokenIdCounter.current() < MAX_SUPPLY, "I'm sorry number of token reached the cap");
         _tokenIdCounter.increment();
         uint256 newItemId = _tokenIdCounter.current();
         _mint(msg.sender, newItemId); //mint the token
         _setTokenURI(newItemId,  tokenURI); //generate the URI
         return newItemId;
     }
+
+    function transferToken(address to, uint256 tokenId) external {
+        require(msg.sender == ownerOf(tokenId), "Sorry, You're not owner of this token!");
+        safeTransferFrom(msg.sender, to, tokenId);
+    }
+
+    function burnToken(uint256 tokenId) external {
+        require(msg.sender == ownerOf(tokenId), "Sorry, You're not owner of this token!");
+        _burn(tokenId);
+    }
+
 }
